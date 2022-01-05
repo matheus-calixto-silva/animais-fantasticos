7cdec913e3bd93e3adbc11fab5e13552cbbd42a3
Matheus Calixto <matheuscalixto8@gmail.com>
2022-01-05 20:09:10 -0300

    implementacao - animacao durante scroll

--- script.js
+++ script.js
@@ -56,3 +56,20 @@
     link.addEventListener("click", scrollToSection);
   });
 })();
+
+(function startScrollAnimation() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        if (section.getBoundingClientRect().top < window.innerHeight * 0.6) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+})();
