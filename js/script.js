edb3981fbe9bdf941caf1b8c3aefcac0630e0573
Matheus Calixto <matheuscalixto8@gmail.com>
2022-01-20 22:47:04 -0300

    implementacao - tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import scrollAnimation from "./modules/scroll-animation.js";
 import accordion from "./modules/accordion.js";
 import tabAnimation from "./modules/tab-animation.js";
 import modal from "./modules/modal.js";
+import tooltip from "./modules/tooltip.js"
 
 scrollSuave();
 scrollAnimation();
 accordion();
 tabAnimation();
 modal();
+tooltip();
