09f1aabaa54be07cf606f08caf7bafb38b62ff6a
Matheus Calixto <matheuscalixto8@gmail.com>
2022-02-04 21:54:52 -0300

    Update scroll-animation.js

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -1,15 +1,19 @@
 export default function startScrollAnimation() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowMetade = window.innerHeight * 0.6;
 
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
   if (sections.length) {
-    function animaScroll() {
-      sections.forEach((section) => {
-        if (section.getBoundingClientRect().top < window.innerHeight * 0.6) {
-          section.classList.add("ativo");
-        }
-      });
-    }
-
     animaScroll();
     window.addEventListener("scroll", animaScroll);
   }
