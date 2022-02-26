841f9dade1f7b9b97ebb3227ce7c9625b382ae85
Matheus Calixto <matheuscalixto8@gmail.com>
2022-02-26 12:14:01 -0300

    refatoracao - tooltip

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tab-nav.js';
 import Modal from './modules/modal.js';
-import tooltip from './modules/tooltip.js'
+import Tooltip from './modules/tooltip.js'
 import dropdownMenu from './modules/dropdown-menu.js';
 import menuMobile from './modules/mobile-menu.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -12,14 +12,19 @@ import scrollAnimation from './modules/scroll-animation.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
+
 const tabNav = new TabNav('[data-tab=menu]  li', '[data-tab=content] section');
 tabNav.init()
+
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
-tooltip();
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 dropdownMenu();
 menuMobile();
 initFuncionamento();
